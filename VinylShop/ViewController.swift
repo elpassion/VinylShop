ab6c8cd0b0d586222335c1da5fda288ba1b11fb2
Jakub Turek <jkbturek@gmail.com>
2019-01-30 14:30:53 +0100

    Add Xcode project

---
+++ VinylShop/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VinylShop
+//
+//  Created by Jakub Turek on 30/01/2019.
+//  Copyright © 2019 EL Passion. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
