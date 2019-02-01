a37057d7d706f4948357d31fffc0b891e1714cda
Jakub Turek <jkbturek@gmail.com>
2019-02-01 14:42:23 +0100

    Spike solution for animations

--- VinylShop/UI/Screens/VinylPage/Animations/ShoppingBoxPresentationAnimator.swift
+++ VinylShop/UI/Screens/VinylPage/Animations/ShoppingBoxPresentationAnimator.swift
@@ -9,6 +9,28 @@ class ShoppingBoxPresentationAnimator: NSObject, UIViewControllerAnimatedTransit
     }
 
     func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
+        // FIXME: Spike solution
+
+        let barController = transitionContext.viewController(forKey: .from)
+            .flatMap { $0 as? UINavigationController }
+            .flatMap { $0.viewControllers.last as? VinylPageController }
+            .map { $0.barController }
+
+        guard let presenting = barController,
+              let presented = transitionContext.viewController(forKey: .to) as? ShoppingBoxController else {
+            return
+        }
+
+        let containerView = transitionContext.containerView
+        containerView.addSubview(presented.view)
+        presented.view.frame = containerView.bounds
+        presented.boxView.dimmedBackgroundView.alpha = 0.0
+
+        UIView.animate(withDuration: transitionDuration(using: transitionContext), animations: {
+            presented.boxView.dimmedBackgroundView.alpha = 1.0
+        }, completion: { _ in
+            transitionContext.completeTransition(!transitionContext.transitionWasCancelled)
+        })
     }
 
 }
